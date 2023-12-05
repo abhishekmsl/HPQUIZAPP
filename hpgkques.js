bdaa92d22a160e9f95c6f1fc7569813b0d81d24e
abhishekmsl <140297159+abhishekmsl@users.noreply.github.com>
2023-12-05 07:02:47 +0530

    Add files via upload

---
+++ hpgkques.js
@@ -0,0 +1,417 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is the largest district (area wise) of Himachal Pradesh?",
+    answer: "Lahaul and Spiti",
+    options: [
+      "Lahaul and Spiti",
+      "Shimla",
+      "Sirmaur",
+      "ChambaSS"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The Chail cricket stadium is located in which district of Himachal Pradesh?",
+    answer: "Solan",
+    options: [
+      "Una",
+      " Solan",
+      "Shimla",
+      "Kangra"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following mountain pass lies in Himachal Pradesh?",
+    answer: "Kangra",
+    options: [
+      "Kangra",
+      " Solan",
+      "Mandi",
+      "Chamba"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In which year Amar Singh Thapa beseiged Kangra Fort?",
+    answer: "1805",
+    options: [
+      "1809",
+      "1805",
+      "1804",
+      "1803",
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where was the Gandhi-Irwin pact signed?",
+    answer: "Shimla ",
+    options: [
+      "kangra",
+      " Chamba",
+      "Dalhousie",
+      "Shimla "
+    ]
+  },
+  {
+    numb: 6,
+    question: "The Pandoh Dam is located on which river in Himachal Pradesh?",
+    answer: "Beas River",
+    options: [
+      "Chenab River",
+      "Ravi River",
+      "Beas River",
+      "Yamuna River"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Who was chosen as the president of the provisional government of Himachal Pradesh formed in 1948?",
+    answer: "Sadanand Chande",
+    options: [
+      "Kanshi Ram",
+      "Sadanand Chande",
+      "Y S Parmar",
+      "Krishnanand"
+    ]
+  },
+
+
+  {
+    numb: 8,
+    question: "Which of the following district of Himachal Pradesh is known as “Chhota Kashi”?",
+    answer: "Mandi",
+    options: [
+      "Mandi",
+      "kullu",
+      "chamba",
+      "kangra"
+    ]
+  },
+
+  {
+    numb: 9,
+    question: "In which year Amar Singh Thapa beseiged Kangra Fort?",
+    answer: "1805",
+    options: [
+      "1804",
+      "1805",
+      "1801",
+      "1815"
+    ]
+  },
+
+  {
+    numb: 10,
+    question: "The water of Kareri Lake in Dharamshala falls into which of the following rivers?",
+    answer: "Layun",
+    options: [
+      "Bhaga",
+      "chakki",
+      "Layun",
+      "chandra"
+    ]
+  },
+
+  {
+    numb: 11,
+    question: "In which district is Gauri Devi ka Tibba peak located?",
+    answer: "Chamba ",
+    options: [
+      "kangra",
+      " Chamba",
+      "Dalhousie",
+      "Shimla "
+    ]
+  },
+
+  {
+    numb: 12,
+    question: "Where in Himachal Pradesh Satluj river enters?",
+    answer: "Shipki  ",
+    options: [
+      "Shipki ",
+      " Namgia",
+      "Karcham",
+      "Keylong "
+    ]
+  },
+
+  {
+    numb: 13,
+    question: "Beas river meets river Satluj at which of the following places?",
+    answer: "Hari ka Patan ",
+    options: [
+      "kangra",
+      " Hari ka Patan",
+      " Mirthal",
+  
+      "Katrain "
+    ]
+  },
+
+  {
+    numb: 14,
+    question: "Who was the founder of Suket state?",
+    answer: "Vir Sen ",
+    options: [
+      "han Sen",
+      " Ban Sen",
+      "Tan Sen",
+      "Vir Sen "
+    ]
+  },
+
+  {
+    numb: 15,
+    question: "Who was the founder of Brahmpur state?",
+    answer: "Meru Verman ",
+    options: [
+      "hari Verman",
+      " Ram Verman",
+      "Meru Verman",
+      "Kumar Verman"
+    ]
+  },
+
+  {
+    numb: 16,
+    question: "In which year were the first elections to Vidhan Sabha held in Himachal Pradesh?",
+    answer: "1951",
+    options: [
+      "1954",
+      " 1951",
+      "1957",
+      "1955 "
+    ]
+  },
+
+  {
+    numb: 17,
+    question: "Who was the first governor of Himachal Pradesh?",
+    answer: " S Chakravarti",
+    options: [
+      "S Chakravarti",
+      "Y S Parmar",
+      "Thakur Ram Singh",
+      "E. Penderal Moon"
+    ]
+  },
+
+  {
+    numb: 18,
+    question: "Nargu sanctuary is located in which district of Himachal Pradesh?",
+    answer: "Mandi ",
+    options: [
+      "kangra",
+      " Chamba",
+      "Dalhousie",
+      "Mandi "
+    ]
+  },
+
+  {
+    numb: 19,
+    question: "Which is the only sanctuary in Himachal Pradesh reporting the presence of Kashmir Stag?",
+    answer: "Gamgul Siahbehi Sanctuary ",
+    options: [
+      "Gamgul Siahbehi Sanctuary",
+      " Kanawar Sanctuary",
+      "Kalatope Khajjiar Sanctuary",
+      " Gamgul Siahbehi Sanctuary "
+    ]
+  },
+
+  {
+    numb: 20,
+    question: "Which of the following districts of Himachal Pradesh is located on the international border of india?",
+    answer: "kinnaur",
+    options: [
+      "kinnaur",
+      " Chamba",
+      "Dalhousie",
+      "Shimla "
+    ]
+  },
+
+  {
+    numb: 21,
+    question: "Who introduced Buddhism in the Lahaul Spiti district of Himachal Pradesh?",
+    answer: "Padmasambhan",
+    options: [
+      " Padmanand",
+      "Padmabhushan",
+      "Padmasambhan",
+      "E. Penderal Moon"
+    ]
+  },
+
+  {
+    numb: 22,
+    question: "Pin Valley National Park is located in which district of Himachal Pradesh?",
+    answer: " Lahaul Spiti",
+    options: [
+      "chamba",
+      " Lahaul Spiti",
+      "kangra",
+      "mandi"
+    ]
+  },
+
+  {
+    numb: 23,
+    question: "Which was the first Capital of Nurpur State?",
+    answer: " Pathan Kot",
+    options: [
+      "Pathan Kot",
+      "shahpur",
+      "trigarta",
+      "niyajpur"
+    ]
+  },
+
+  {
+    numb: 24,
+    question: "The longest river of Himachal Pradesh is",
+    answer: "  Satluj",
+    options: [
+      "Jehlem",
+      "Chenab",
+      " Beas",
+      " Satluj"
+    ]
+  },
+
+  {
+    numb: 25,
+    question: "When were many areas of Punjab merged with Himachal Pradesh?",
+    answer: " 1966",
+    options: [
+      "1966",
+      "1961",
+      "1964",
+      "1965"
+    ]
+  },
+
+  {
+    numb: 26,
+    question: "What is the area of Himachal Pradesh?",
+    answer: "21,495 sq. mi",
+    options: [
+      "24,495 sq. mi",
+      "21,495 sq. mi",
+      "25,495 sq. mi",
+      "27,495 sq. mi"
+    ]
+  },
+
+  {
+    numb: 27,
+    question: "Himachal Pradesh came into being on 15th April 1948 and had four districts. At that time which one among the following districts was not the part of Himachal?",
+    answer: "Bilaspur ",
+    options: [
+      "Bilaspur",
+      "Chamba",
+      "sirmaur",
+      "kangra"
+    ]
+  },
+
+
+  {
+    numb: 28,
+    question: "Which one is is the largest district by area wise in Himachal Pradesh",
+    answer: "  Lahaul and Spiti",
+    options: [
+      "kinnaur",
+      "Kangra",
+      "Shimla",
+      " Lahaul and Spiti"
+    ]
+  },
+
+
+  {
+    numb: 29,
+    question: "Mukkila Glacier is located in which one of the following regions?",
+    answer: " Bhaga Valley",
+    options: [
+      "Bhaga Valley",
+      "Pangi",
+      "Manali",
+      "Rohru"
+    ]
+  },
+
+
+  {
+    numb: 30,
+    question: "Who was the founder of Theog princely state?",
+    answer: " Jai Chand",
+    options: [
+      " Ram Chand",
+      "Jai Chand",
+      "Veer chand",
+      "None of the above"
+    ]
+  },
+
+  {
+    numb: 31,
+    question: "Manikaran hot water springs' are present in which district of Himachal Pradesh?",
+    answer: " Kullu",
+    options: [
+      "Kullu",
+      "Kangra",
+      "Mandi",
+      "Chamba"
+    ]
+  },
+
+
+  {
+    numb: 32,
+    question: "Who was the first governor-general of India to visit Shimla?",
+    answer: " Lord Amherst",
+    options: [
+      "Lord Amherst",
+      "Lord Mayo",
+      "Lord Cornwallis",
+      "Lord Dalhousie"
+    ]
+  },
+
+
+
+  {
+    numb: 33,
+    question: "The State Day of Himachal Pradesh is",
+    answer: " 5th April",
+    options: [
+      "15th January",
+      "5th April",
+      "5 March",
+      "21 March"
+    ]
+  },
+
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
